2a8ccd9e95a40618f453e0d25ef933cf28539688
Sean Marjason <smarj1@hotmail.com>
2021-09-22 23:38:11 +0100

    Add title pane to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import './pages.css'
+import './Home.css'
+import { Helmet } from 'react-helmet'
+import NavigationButton from '../components/NavigationButton/NavigationButton'
+
 
 const Home = () => {
   return(
-    <div className="page">
-      <h3 id="home" className="hidden">Home</h3>
+    <div id="homePage" className="page">
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>  
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/> 
+      </Helmet>
+      <div id="headline">
+        <h1 id="home" className="">Sean Marjason</h1>
+        <p id="tagline">Web Developer | Analyst | Technologist</p>
+        <NavigationButton text="View Portfolio" link="#projects"/>
+        <NavigationButton text="Contact Me" link="#contact"/>
+      </div>
+      <div id="homeGraphic">
+
+      </div>
     </div>
   );
 }
