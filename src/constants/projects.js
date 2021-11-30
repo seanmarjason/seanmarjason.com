5089492139a3d31fe884f8ecaded6a480763ccd8
Sean Marjason <smarj1@hotmail.com>
2021-11-30 22:42:17 +0000

    Add content and assets for projects

---
+++ src/constants/projects.js
@@ -0,0 +1,29 @@
+import gdesiImg from '../assets/projects/projects-gdesi.png'
+import seanmarjasonImg from '../assets/projects/projects-seanmarjason.png'
+
+export const projects = [
+  {
+    title: 'Gdesi',
+    shortDescription: 'No longer want to be tied to an office? Well, we\'re working on it!',
+    longDescription: 
+      `Realising the potential of remote work, I set out to uncover why more 
+      companies have not moved to a fully remove working model. This site aims 
+      to be an ongoing investigation into the reasons for a lack of movement 
+      towards remote working as standard, and what needs to be done to make it 
+      a reality`,
+    link: 'https://www.gdesi.io/',
+    image: gdesiImg,
+  },
+  {
+    title: 'Personal Website',
+    shortDescription: 'I needed somewhere to showcase my projects, so I created my own.',
+    longDescription: 
+      `The site you're looking at RIGHT now I just had to build myself. While 
+      I had a portfolio site in the past, it was done back when I only knew 
+      basic HTML, CSS, and Javascript. It definitely needed an upgrade. What you 
+      see now comes from my experience working with ReactJS, as well as various 
+      experiments I needed a place to do.`,
+    link: 'https://www.seanmarjason.com/',
+    image: seanmarjasonImg,
+  },
+]
