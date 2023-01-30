bd91ebe0ff64f6c85a5ff0cd1ac32751098c3b21
Sean Marjason <sean.marjason@bjss.com>
2023-01-30 08:07:40 +0000

    Remove university projects

--- src/projects/index.js
+++ src/projects/index.js
@@ -1,13 +1,13 @@
 import project01 from './2019-12_HTMLWebsite';
-import project02 from './2020-10_GameProject';
-import project03 from './2021-01_MusicAnalyzer';
+// import project02 from './2020-10_GameProject';
+// import project03 from './2021-01_MusicAnalyzer';
 import project04 from './2021-03_Gdesiio';
 import project05 from './2021-11_SeanMarjasoncom';
 
 const projects = [
   project01,
-  project02,
-  project03,
+  // project02,
+  // project03,
   project04,
   project05,
 ]
