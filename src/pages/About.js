4385993b23d9faa5348556b66f06e8dd87f723af
Sean Marjason <smarjason@outlook.com>
2023-07-25 21:37:14 +0100

    Update skills content and prep for dual skill approach

--- src/pages/About.js
+++ src/pages/About.js
@@ -8,19 +8,39 @@ import AnalysisImage from '../assets/Business-Analysis-Skills_user-journey-with-
 
 const skills = [
   {
-    title: 'Product Analysis',
+    title: 'Product Management & Analysis',
     image: AnalysisImage,
-    text: 'Idea not fully formed yet? Know you need to put something together, but can\'t work out how to make it sustainable, valuable, or align with your user needs? Bringing years of experience in Business and Systems Analysis, I can help you to uncover user needs and organise them into contained stories ready to be refined and delivered.'
+    text: `
+      Want to create something new but haven't identified your 
+      market or its needs? Have an app idea but haven't fully 
+      refined it or found a way to scale up a sustainable product? 
+      Bringing years of experience in Business and Systems Analysis, 
+      I can help you to uncover user needs and organise them into 
+      well defined product features ready to deliver user value.
+    `
   },
   {
-    title: 'Development',
+    title: 'Software Engineering',
     image: DevelopmentImage,
-    text: 'Looking for a new website? Need something more complex than static content that you could do on Wix, Squarespace, or Wordpress? I can help you put together a lightweight website suiting your needs with market standard web frameworks (ReactJS, Node, AWS).'
+    text: `
+      Looking for a new website? Need something more complex 
+      than static content that you could do on Wix, Squarespace, 
+      or Wordpress? I can help you put together a lightweight 
+      website or API suiting your needs with market standard web 
+      frameworks (ReactJS, Node, AWS).
+    `
   },
   {
-    title: 'Design',
+    title: 'Product & Web Design',
     image: DesignImage,
-    text: 'Have an idea but not sure how to put together a product or service that will appeal to your users? Taking an iterative approach, I can help you put together mockups to test your idea, or to lay out your application ready for development using platforms easily consumed by your developers (Sketch, Invision Studio, Adobe XD)'
+    text: `
+      Have an idea but not sure how to put together a product 
+      or service that will appeal to your users? Taking an 
+      iterative approach, I can help you put together mockups 
+      to test your idea, or to lay out your application ready 
+      for development using platforms easily consumed by your 
+      developers (Sketch, Invision Studio, Adobe XD)
+    `
   },
 ]
 
