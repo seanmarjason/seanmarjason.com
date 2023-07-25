4310b6d5437da74ce3e12ac897038ac0446c3119
Sean Marjason <smarjason@outlook.com>
2023-07-25 23:12:35 +0100

    Add projects toggle

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './pages.css';
 import './Projects.css';
 import devProjects from '../projects/development';
@@ -5,19 +6,37 @@ import productProjects from '../projects/product';
 import ProjectCard from '../components/ProjectCard/ProjectCard'
 
 const Projects = () => {
+
+const [active, setActive] = useState('Product')
+
   return(
     <div className="page" id="projects-page">
       <h3 id="projects">Projects</h3>
+      <div id="project-buttons-container">
+      {
+        ['Product', 'Development'].map(element => 
+          <button 
+            className="projects-button"
+            disabled={active === element}
+            onClick={() => setActive(element)}
+          >
+            {element}
+          </button>
+        )
+      }
+      </div>
       <div id="project-links">
         {
-          devProjects.map((project, index) => 
-            <ProjectCard project={project} key={index} />
-          )
+          active === 'Product' &&
+            productProjects.map((project, index) => 
+              <ProjectCard project={project} key={index} />
+            )
         }
         {
-          productProjects.map((project, index) => 
-            <ProjectCard project={project} key={index} />
-          )
+          active === 'Development' &&
+            devProjects.map((project, index) => 
+              <ProjectCard project={project} key={index} />
+            )
         }
       </div>
     </div>
